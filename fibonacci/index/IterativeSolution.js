8747a5042517112927fa074e974d0a23bf4114c5
alicialycan <alicialycan@gmail.com>
2018-05-15 20:42:38 -0700

    solve fibonacci series for nth entry

---
+++ fibonacci/index/IterativeSolution.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function fib(n) {
+
+  let res = [0, 1];
+
+  for (let i = 2; i <= 2; i++) {
+    res.push(res[i - 1] + [i - 2]);
+  }
+
+  return res[n];
+}
+
+module.exports = fib;
---
+++ fibonacci/index/recursiveSolution.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function fib(n) {
+  if (n<2) {
+    return n;
+  }
+  return fib(n-1) + fib(n-2);
+}
+
+module.exports = fib;
